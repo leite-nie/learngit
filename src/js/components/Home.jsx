a7614568111218b92817cba636a4d3f7585291a0
nieyunen <nieyunen@globalegrow.com>
2018-07-31 08:49:59 +0800

    添加高阶组件

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,21 +1,100 @@
 import { connect } from 'react-redux';
 
-import React from 'react';
+import React,{Component} from 'react';
 
 
-class Home extends React.Component{
+class Home extends Component{
     render(){
         const { number, setIncrease, setDecrease } = this.props;
         return(
             <div>
                 <h2>welcome my home! this.props.number : {this.props.number}</h2>
-                <div className="test2">
-                    <div className="test2-child">我要垂直居中哦</div>
-                </div>
+                <GoodBye></GoodBye>
+                <Welcome></Welcome>
             </div>
         )
     }
 }
+
+const  wrapWithUsername = (WrappedComponent) =>{
+    class NewComponent extends Component {
+        constructor() {
+            super();
+            this.state = {
+                username: ''
+            }
+        }
+
+        componentWillMount() {
+            let username = localStorage.getItem('username');
+            this.setState({
+                username: username
+            })
+        }
+
+        render() {
+            return <WrappedComponent username={this.state.username}/>
+        }
+    }
+
+    return NewComponent
+}
+
+
+
+class Welcome  extends Component{
+    /*constructor(){
+        super();
+        this.state = {
+            userName : ""
+        }
+    }
+    componentDidMount(){
+        let _name = localStorage.getItem("username");
+        this.setState({
+            userName : _name
+        })
+    }
+    render(){
+        return(
+            <div>welcome {this.state.userName}</div>
+        )
+    }*/
+    render() {
+        return (
+            <div>welcome {this.props.username}</div>
+        )
+    }
+}
+Welcome = wrapWithUsername(Welcome);
+class GoodBye  extends Component{
+    /*constructor(){
+        super();
+        this.state = {
+            userName : ""
+        }
+    }
+    componentDidMount(){
+        let _name = localStorage.getItem("username");
+        this.setState({
+            userName : _name
+        })
+    }
+    render(){
+        return(
+            <div>GoodBye {this.state.userName}</div>
+        )
+    }*/
+    render() {
+        return (
+            <div>goodbye {this.props.username}</div>
+        )
+    }
+}
+GoodBye = wrapWithUsername(GoodBye);
+
+
+
 
 function mapStateToProps(state) {
     return {
